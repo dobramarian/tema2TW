bc00ade593592d824b294505a879a9f6c6863eb2
dobramarian <72127982+dobramarian@users.noreply.github.com>
2020-11-28 22:27:27 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,5 +1,19 @@
 function addTokens(input, tokens){
-    
+    if(typeof input !== 'string') throw Error('Invalid input');
+     if(input.length < 6) throw Error('Input should have at least 6 characters');
+     for(let item of tokens)
+     {
+        if(typeof item.tokenName === 'string' )
+        {
+        if(input.includes("..."))
+            return input.replace('...','${'+item.tokenName+'}');
+            else
+            return input;
+        }
+        else
+        throw Error('Invalid array format');
+     }
+
 }
 
 const app = {
